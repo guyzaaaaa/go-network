fec65bdc9de9642c75d22c46e2225c832aaa111a
guyzaaaaa <151008388+guyzaaaaa@users.noreply.github.com>
2023-11-21 13:38:04 +0700

    Add files via upload

---
+++ tutor2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello World")
+	})
+	
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello Worldr")
+	})
+
+fs:=http.FileServer(http.Dir("static/"))
+http.Handle("/static/", http.StripPrefix("/static/", fs))
+	
+http.ListenAndServe(":8080", nil)
+}
