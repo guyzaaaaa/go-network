b52a06de3ea6877d1f7c7cb43fbd709d806d502c
guyzaaaaa <641463019@crru.ac.th>
2023-11-23 18:55:34 +0700

    Add new Server and Client

---
+++ go-chat/Client/client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:5000")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to server")
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		// Read user input
+		fmt.Print("Enter message: ")
+		message, _ := reader.ReadString('\n')
+
+		// Send the message to the server
+		conn.Write([]byte(message))
+
+		// Print the number of bytes sent
+		fmt.Printf("Sent %d bytes\n", len(message))
+
+		// Check for the exit condition
+		if strings.TrimSpace(message) == "quit" {
+			fmt.Println("Exiting the program.")
+			return
+		}
+
+		// Receive and print the server's response
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			return
+		}
+		fmt.Printf("Server response: %s", buffer[:n])
+	}
+}
